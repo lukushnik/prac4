99b1175bff0e404ce08169579d9f59458d9df886
Katya Lukash <lukushnik@gmail.com>
2023-11-15 20:59:17 +0200

    add types and few children methods

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+from typing import List
+
+
 class Animal:
-    def __init__(self, name, age):
+    def __init__(self, name: str, age: int):
         self.name = name
         self.age = age
         print("Animal constructor called.")
@@ -7,42 +10,62 @@ class Animal:
     def __del__(self):
         print("Animal destructor called.")
 
-    def make_sound(self):
+    def make_sound(self) -> None:
         pass
 
 
 class Dog(Animal):
-    def __init__(self, name, age, breed):
+    def __init__(self, name: str, age: int, breed: str):
         super().__init__(name, age)
         self.breed = breed
 
-    def __del__(self):
-        print("Cat destructor.")
+    def is_puppy(self) -> bool:
+        return self.age < 1
 
-    def make_sound(self):
+    def display_info(self) -> None:
+        print(f"{self.name} is a {self.age}-year-old {self.breed}.")
+
+    def make_sound(self) -> None:
         print("Woof! Woof!")
+
+    def __del__(self):
+        print("Dog destructor.")
 
 
 class Cat(Animal):
-    def __init__(self, name, age, is_indoor):
+    def __init__(self, name: str, age: int, is_indoor: bool):
         super().__init__(name, age)
         self.is_indoor = is_indoor
+
+    def is_kitten(self) -> bool:
+        return self.age < 1
+
+    def display_info(self) -> None:
+        indoor_status = "indoor" if self.is_indoor else "outdoor"
+        print(f"{self.name} is a {self.age}-year-old {indoor_status} cat.")
+
+    def make_sound(self) -> None:
+        print("Meow! Meow!")
 
     def __del__(self):
         print("Cat destructor.")
 
-    def make_sound(self):
-        print("Meow! Meow!")
 
-
-def main():
-    animals = [Dog("Buddy", 3, "Labrador"), Cat("Whiskers", 2, True), Dog("Max", 4, "German Shepherd")]
+def main() -> None:
+    animals: List[Animal] = [Dog("Hasky", 3, "Labrador"), Cat("Orange", 2, True), Dog("Max", 4, "TestBreed")]
 
     for animal in animals:
         animal.make_sound()
 
-    # Виведення результатів
+    # Displaying information
     for animal in animals:
+        if isinstance(animal, Dog):
+            animal.display_info()
+            print(f"Is a puppy: {animal.is_puppy()}")
+        elif isinstance(animal, Cat):
+            animal.display_info()
+            print(f"Is a kitten: {animal.is_kitten()}")
+
         del animal
 
 
